c11130706564e0c65a3242d579380844bf635afe
Emonique <85489511+Emonique@users.noreply.github.com>
2025-06-10 23:45:16 +0100

    Update __init__.py

--- Tyon_geoscience_AI/core/__init__.py
+++ Tyon_geoscience_AI/core/__init__.py
@@ -1,6 +1,6 @@
 # core/__init__.py
 from .entropy_calc import EntropyCalculator
-from .fractal_analysis import FractalAnalyzer
+from .fractal_analysis import FractalAnalyzer, compute_fractal_dimension
 from .rqi_model import ReservoirQualityIndex
 from .trap_predictor import ZoneDetector
 from .drilling_efficiency import DrillingEfficiencyPredictor
