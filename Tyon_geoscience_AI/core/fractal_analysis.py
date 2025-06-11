0bdd729cf07b083c4242f53948e477e9292aec90
Emonique <85489511+Emonique@users.noreply.github.com>
2025-06-11 01:33:11 +0100

    Update fractal_analysis.py

--- Tyon_geoscience_AI/core/fractal_analysis.py
+++ Tyon_geoscience_AI/core/fractal_analysis.py
@@ -20,33 +20,6 @@ class FractalAnalyzer:
         except:
             return np.nan
 
-# New function to resolve AttributeError
-def compute_fractal_dimension(data, min_samples=5):
-    """Compute fractal dimension using FractalAnalyzer class"""
-    analyzer = FractalAnalyzer(min_samples=min_samples)
-    return analyzer.calculate(data)import numpy as np
-from sklearn.linear_model import LinearRegression
-
-class FractalAnalyzer:
-    """Fractal dimension analysis for subsurface characterization"""
-    
-    def __init__(self, min_samples=5):
-        self.min_samples = min_samples
-        
-    def calculate(self, data):
-        """Compute fractal dimension (Hurst exponent)"""
-        if len(data) < self.min_samples:
-            return np.nan
-            
-        try:
-            x = np.arange(1, len(data)+1).reshape(-1,1)
-            y = np.log(np.array(data) + 1e-6)
-            model = LinearRegression().fit(np.log(x), y)
-            return -model.coef_[0][0]  # Negative slope = fractal dimension
-        except:
-            return np.nan
-
-# New function to resolve AttributeError
 def compute_fractal_dimension(data, min_samples=5):
     """Compute fractal dimension using FractalAnalyzer class"""
     analyzer = FractalAnalyzer(min_samples=min_samples)
